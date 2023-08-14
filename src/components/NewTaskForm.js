ecdaa7e4f48a86c938fb2667f26b3de9ea48fa7b
Farida NDiaye <faridandiaye60@gmail.com>
2023-08-14 18:49:25 -0400

    REACT HOOK EVENTS MINI PROJECT

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,27 +1,3 @@
-// import React from "react";
-
-// function NewTaskForm() {
-//   return (
-//     <form className="new-task-form">
-//       <label>
-//         Details
-//         <input type="text" name="text" />
-//       </label>
-//       <label>
-//         Category
-//         <select name="category">
-//           {/* render <option> elements for each category here */}
-//         </select>
-//       </label>
-//       <input type="submit" value="Add task" />
-//     </form>
-//   );
-// }
-
-// export default NewTaskForm;
-
-
-
 import React, { useState } from "react";
 
 function NewTaskForm({ categories, onTaskFormSubmit }) {
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,3 @@
-// import React from "react";
-
-// function Task() {
-//   return (
-//     <div className="task">
-//       <div className="label">CATEGORY HERE</div>
-//       <div className="text">TEXT HERE</div>
-//       <button className="delete">X</button>
-//     </div>
-//   );
-// }
-
-// export default Task;
-
-
-
 import React from "react";
 
 function Task({ text, category, onDelete }) {
